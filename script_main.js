545b4afb56d4c436d86b59bb5549982b35875969
João Pedro MW <80231608+Joao-Pedro-MW@users.noreply.github.com>
2022-10-22 23:55:26 -0300

    Add files via upload

---
+++ script_main.js
@@ -0,0 +1,10 @@
+function mostrar_mapa(){window.open("https://goo.gl/maps/qgUfGgAUJruPVq7y9")}
+
+function openNav() {
+  document.getElementById("MENU").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+}
+function closeNav() {
+  document.getElementById("MENU").style.width = "0";
+  document.getElementById("main").style.marginLeft= "0";
+}
